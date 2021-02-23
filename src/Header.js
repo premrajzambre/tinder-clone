f226c4e1b01918e5c096f585c14c5cb85900b3fc
Premraj <premrajzambre@gmail.com>
2021-02-23 20:30:25 +0530

    Header Complete

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+
+            <img 
+                className="header__logo"
+                src="https://toppng.com/uploads/preview/tinder-logo-transparent-11563247829tbb8yoy9a1.png"
+                alt="Tinder Logo"
+            />
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header
