ba45252dff8597355324c0f5a913d232a810e802
Premraj <premrajzambre@gmail.com>
2021-02-24 20:19:14 +0530

    Tinder cards created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import TinderCards from './TinderCards';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* Header */}
       <Header />
       {/* TinderCards */}
+      <TinderCards />
       {/* SwipeButtons */}
     </div>
   );
