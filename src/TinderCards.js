ada7beb1ebfc0d2911d1fbeb572fbf8065950bc9
Premraj <premrajzambre@gmail.com>
2021-02-25 20:08:27 +0530

    tinder-backend attached

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,18 +1,20 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import TinderCard from 'react-tinder-card';
+import axios from "./axios";
 import "./TinderCards.css";
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name : "Elon Musk",
-            url : "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1398%26cropX2%3D3908%26cropY1%3D594%26cropY2%3D3102",
-        },
-        {
-            name : "Jeff Bezos",
-            url : "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D560%26cropX2%3D1783%26cropY1%3D231%26cropY2%3D1455",
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData () {
+            const req = await axios.get('/tinder/cards');
+
+            setPeople(req.data);
         }
-    ]);
+
+        fetchData();
+    }, [])
 
     const swiped = (direction, nameToDelete) => {
         console.log("removing : " + nameToDelete);
@@ -36,7 +38,7 @@ function TinderCards() {
                         onCardLeftScreen={() => outOfFrame(person.name)}
                     >
                         <div
-                            style={{ backgroundImage: `url(${person.url})`}}
+                            style={{ backgroundImage: `url(${person.imgUrl})`}}
                             className = "card"
                         >
                             <h3>{person.name}</h3>
